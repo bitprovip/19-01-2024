f8e97b2c57543f318fb719c690ca69cb29da4db9
bitprovip <93476900+bitprovip@users.noreply.github.com>
2024-01-19 14:05:10 +0700

    upcode

---
+++ backend/src/migrations/migrations/migrate-booking.js
@@ -0,0 +1,59 @@
+'use strict';
+module.exports = {
+  // parkingid: DataTypes.INTEGER,
+  //   userid: DataTypes.INTEGER,
+  //   name: DataTypes.STRING,
+  //   phone: DataTypes.STRING,
+  //   email: DataTypes.STRING,
+  //   bsx: DataTypes.STRING,
+  //   stt: DataTypes.INTEGER,
+  //   thoigian: DataTypes.INTEGER,
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Booking', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      parkingid: {
+        type: Sequelize.INTEGER
+      },
+      userid: {
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      bsx: {
+        type: Sequelize.STRING
+      },
+      stt: {
+        type: Sequelize.INTEGER
+      },
+      thoigian: {
+        type: Sequelize.INTEGER
+      },
+      bookingcode: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Booking');
+  }
+};
